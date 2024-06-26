b6d512530023e639c0861f5156ef7bafb957b2b4
Mirabueno-Lanz-Manuel-BSCS32E1 <162534195+Mirabueno-Lanz-Manuel-BSCS32E1@users.noreply.github.com>
2024-06-26 20:03:31 +0800

    Add files via upload

---
+++ Project.cs
@@ -0,0 +1,7 @@
+﻿private void Project_Load(object sender, EventArgs e)
+{
+    Label helloLabel = new Label();
+    helloLabel.Text = "Hello ";
+    helloLabel.Location = new Point(50, 50);
+    this.Controls.Add(helloLabel);
+}
